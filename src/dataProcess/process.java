49e0fa3795e48e93fa34470d0a6d9822345d38ac
motu555 <450555825@qq.com>
2018-07-08 20:11:08 +0800

    social INFO

--- src/dataProcess/process.java
+++ src/dataProcess/process.java
@@ -6,10 +6,7 @@ import data.Post;
 
 import java.io.*;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.Locale;
+import java.util.*;
 
 /**
  * Created by motu on 2018/6/17.
@@ -69,25 +66,44 @@ public class process {
      * 按照每个用户的最后几个切分？
      */
     public static void DivideData(String filepath){
-        String filename = "DianpingCheckinfalse2525.txt";
-        String readpath = filepath+filename;
+        String filename = "DianpingCheckinfalse1010";
+        String readpath = filepath+filename +".txt";
+        String despth = "./demo/data/UTP/";
 
         FileInputStream file;
         BufferedReader bufferedReader;
         StringBuilder trainStr = new StringBuilder();
         StringBuilder testStr = new StringBuilder();
         String read;
+        Map<String, Integer> userIds, poiIds, timeIds;//用于编号的映射关系
+        userIds = new HashMap<>();
+        poiIds = new HashMap<>();
+        timeIds = new HashMap<>();
         int trainnum=0;
         int testnum=0;
         try {
             file = new FileInputStream(readpath);
             bufferedReader = new BufferedReader(new InputStreamReader(file, "UTF-8"));
             while ((read = bufferedReader.readLine()) != null) {
+                String[] contents = read.trim().split("[ \t,]+");
+                String tempUser = contents[0];
+                String tempItem = contents[1];
+                String tempTime = contents[2];
+                //重新映射编号
+                int innerUser = userIds.containsKey(tempUser) ? userIds.get(tempUser) : userIds.size();
+                userIds.put(tempUser, innerUser);
+                int innerPoi = poiIds.containsKey(tempItem) ? poiIds.get(tempItem) : poiIds.size();
+                poiIds.put(tempItem, innerPoi);
+                int innerTime = timeIds.containsKey(tempTime) ? timeIds.get(tempTime) : timeIds.size();
+                timeIds.put(tempTime, innerTime);
+                String writeStr = innerUser+"\t"+innerTime+"\t"+innerPoi+"\n";
                 if(Math.random()<0.8){
-
+                    trainStr.append(writeStr);
+                    trainnum++;
                 }
                 else {
-
+                    testStr.append(writeStr);
+                    testnum++;
                 }
 
             }
@@ -98,17 +114,55 @@ public class process {
         } catch (IOException e) {
             e.printStackTrace();
         }
+        System.out.println("trainnum:"+trainnum+" testnum:   "+testnum);
+        System.out.println("usernum:"+userIds.size()+" poinum:"+poiIds.size()+" timenum:"+timeIds.size());
+        FileOperation.writeNotAppdend(despth + filename + "_train.txt", trainStr.toString());
+        FileOperation.writeNotAppdend(despth + filename + "_test.txt", trainStr.toString());
+        //id映射文件记录
+        FileOperation.writeNotAppdend(filepath+filename+"_userMapIndex",userIds.toString());
+        FileOperation.writeNotAppdend(filepath+filename+"_poiMapIndex",poiIds.toString());
+        FileOperation.writeNotAppdend(filepath+filename+"_timeMapIndex",timeIds.toString());
 
+    }
+    public static void getuserMap(String filepath){
+        String filename = "DianpingCheckinfalse1010";
+        String readpath = filepath+filename +".txt";
+        String despth = "./demo/data/UTP/";
+        FileInputStream file;
+        BufferedReader bufferedReader;
+        StringBuilder trainStr = new StringBuilder();
+        StringBuilder testStr = new StringBuilder();
+        String read;
+        Map<String, Integer> userIds;//用于编号的映射关系
+        userIds = new HashMap<>();
+        try {
+            file = new FileInputStream(readpath);
+            bufferedReader = new BufferedReader(new InputStreamReader(file, "UTF-8"));
+            while ((read = bufferedReader.readLine()) != null) {
+                String[] contents = read.trim().split("[ \t,]+");
+                String tempUser = contents[0];
+                //重新映射编号
+                int innerUser = userIds.containsKey(tempUser) ? userIds.get(tempUser) : userIds.size();
+                userIds.put(tempUser, innerUser);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        FileOperation.writeNotAppdend(filepath+filename+"_userMap",userIds.toString());
 
     }
 
 
     public static void main(String args[]){
 //        timestampTrans("./rawdata");
-            DivideData("./rawdata/");
+//            DivideData("./rawdata/");
+        getuserMap("./rawdata/");
 //        String date = Date2TimeStamp("15-09-24" , "yy-MM-dd");// HH:mm:ss
 //        String date =TimeStamp2Date( "1288291694000","yy-MM-dd");
-//        System.out.print(date);
     }
 
 }
--- src/test/PIFTUTP.java
+++ src/test/PIFTUTP.java
@@ -14,28 +14,28 @@ import java.util.List;
 public class PIFTUTP {
     public static void main(String[] args) throws IOException {
 //        String dataName = "lastFM";
-        String dataName = "Dianping_Time";
-//        String fileName = "checkin_pitf";
-        int coreNum = 1;
-//        List<Post> trainPosts = DataReader.readPosts("./demo/data/" + dataName + "/" + fileName + "_train");
-//        List<Post> testPosts = DataReader.readPosts("./demo/data/" + dataName + "/" + fileName + "_test");
-        List<Post> trainPosts = DataReader.readPosts("./demo/data/" + dataName + "/"  + "_train");
-        List<Post> testPosts = DataReader.readPosts("./demo/data/" + dataName + "/"  + "_test");
+        String dataName = "DianpingCheckinfalse2525";
+        String fileName = "UTP";
+
+        List<Post> trainPosts = DataReader.readPostUTP("./demo/data/"  + fileName + "/"+ dataName  + "_train.txt");
+        List<Post> testPosts = DataReader.readPostUTP("./demo/data/"  + fileName + "/"+ dataName  + "_test.txt");
+//        List<Post> trainPosts = DataReader.readPosts("./demo/data/" + dataName + "/"  + "_train");
+//        List<Post> testPosts = DataReader.readPosts("./demo/data/" + dataName + "/"  + "_test");
         System.out.println("PITFTest");
-        System.out.println(dataName + " " + coreNum);
-        int dim = 64;
-        double initStdev = 0.01;
+        System.out.println(dataName);
+        int dim = 64;//纬度
+        double initStdev = 0.01;//初始化的赋值方差
         int iter = 20;//100
-        double learnRate = 0.05;
+        double learnRate = 0.05;//
         double regU = 0.00005;
-        double regI = regU, regT = regU;
+        double regI = regU, regT = regU;//lameta
         int numSample = 100;
         int randomSeed = 1;
 
         PITF pitf = new PITF(trainPosts, testPosts, dim, initStdev, iter, learnRate, regU, regI, regT, randomSeed, numSample);
         pitf.run();//这个函数会调用model的初始化及创建函数
         System.out.println("dataName:\t" + dataName);
-        System.out.println("coreNum:\t" + coreNum);
+//        System.out.println("coreNum:\t" + coreNum);
 
     }
 }
